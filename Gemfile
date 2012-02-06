67b6ff8f3e5ba7928236e6be525b528713d44164
Alex Godin <alex@alexgodin.com>
2012-02-05 21:54:24 -0500

    init

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# A sample Gemfile
+source "http://rubygems.org"
+
+ gem "sinatra"
+ gem "aws-s3"
+ gem "json"
---
+++ app.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+require 'aws/s3'
+require 'json'
+
+include AWS::S3
+
+get '/bomb/:count' do |count|
+  connect_to_s3
+  corgis = Bucket.objects('Corgi')
+  randomcorgis = corgis.shuffle![1..[corgis.count,count.to_i].min]
+  corgi_urls = randomcorgis.map {|corgi| corgi.url}
+  corgi_urls.to_json
+end
+
+get '/random' do
+  connect_to_s3
+  corgis = Bucket.objects('Corgi')
+  randomcorgis = corgis[rand(corgis.count)].url
+end
+
+private
+
+def connect_to_s3
+  Base.establish_connection!(
+    :access_key_id     => 'AKIAJ3R5C5IX2UYKYTEA',
+    :secret_access_key => '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'
+  )
+end
